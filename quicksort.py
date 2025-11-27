7ad232cd0abd9e5b5bc0bdb22bababece6b5e6a2
Tonny Blair <jusanotherthursday@gmail.com>
2025-11-27 23:12:46 +0300

    quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,22 @@
+def quicksort(arr, low, high):
+    if low < high:
+        #partition
+        pi = partition(arr, low, high)
+        #recurse
+        quicksort(arr, low, pi - 1)
+        quicksort(arr, pi + 1, high)
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+    
+array = [64, 34, 25, 12, 22, 11, 90]
+
+quicksort(array, 0, len(arr) - 1)
+
+print(array)
