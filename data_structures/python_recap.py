a7883e545fb157b4c99b9312f629602fb0c0021c
Tonny Blair <jusanotherthursday@gmail.com>
2025-12-08 21:56:45 +0300

    Update python_recap.py

--- data_structures/python_recap.py
+++ data_structures/python_recap.py
@@ -4,3 +4,12 @@ arr2 = [x*x for x in arr if x % 2 == 0]
 print(f"Original array: {arr}")
 print(f"Updated array: {arr2}")
 
+#Dictionary counting
+s = "abracadabra"
+word_count = {}
+for c in s:
+    if c in word_count:
+        word_count[c] += 1
+    else:
+        word_count[c] = 1
+print(word_count)
