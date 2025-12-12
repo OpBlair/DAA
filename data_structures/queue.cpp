f6b0f7242202fefe556ec2a72775611d7ae236ea
Tonny Blair <jusanotherthursday@gmail.com>
2025-12-12 23:12:35 +0300

    Create queue.cpp

---
+++ data_structures/queue.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+
+    cout << "Front element: " << q.front() << endl;
+    q.pop();
+    cout << "Front after pop: " << q.front() << endl;
+
+    return 0;
+}
